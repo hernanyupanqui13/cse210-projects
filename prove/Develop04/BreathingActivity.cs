89abdb0933c6f75f2e6f9e52faa8fa1a331bca46
hernanyupanqui13 <yup19001@byui.edu>
2024-05-14 11:02:45 -0500

    General structure created

---
+++ prove/Develop04/BreathingActivity.cs
@@ -0,0 +1,12 @@
+public class BreathingActivity : Activity
+{
+    public BreathingActivity()
+    {
+        this._description = "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.";
+    }
+
+    public void Run()
+    {
+
+    }
+}
